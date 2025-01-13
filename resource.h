ec237342d6514f392e17301e3160d3b86d4cbf03
Ilia Chakarov <chakarov.ilia@gmail.com>
2025-01-13 12:25:56 +0200

    Add project files.

---
+++ resource.h
@@ -0,0 +1,40 @@
+//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ generated include file.
+// Used by PS_All.rc
+//
+#define IDC_MYICON                      2
+#define IDD_PSALL_DIALOG                102
+#define IDS_APP_TITLE                   103
+#define IDD_ABOUTBOX                    103
+#define IDM_ABOUT                       104
+#define IDM_EXIT                        105
+#define IDI_PSALL                       107
+#define IDI_SMALL                       108
+#define IDC_PSALL                       109
+#define IDR_MAINFRAME                   128
+#define IDD_DIALOG1                     129
+#define IDC_BUTTON_CHECK                1000
+#define IDC_CHECK1                      1001
+#define IDC_CHECK2                      1002
+#define IDC_CHECK3                      1003
+#define IDC_RADIO1                      1004
+#define IDC_RADIO2                      1005
+#define IDC_EDIT1                       1006
+#define IDC_RADIO3                      1007
+#define IDC_RADIO4                      1008
+#define IDC_CHECK4                      1009
+#define IDC_CHECK_HIDDEN                1009
+#define ID_FILE_DIALOG                  32771
+#define IDC_STATIC                      -1
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NO_MFC                     1
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32774
+#define _APS_NEXT_CONTROL_VALUE         1010
+#define _APS_NEXT_SYMED_VALUE           110
+#endif
+#endif
